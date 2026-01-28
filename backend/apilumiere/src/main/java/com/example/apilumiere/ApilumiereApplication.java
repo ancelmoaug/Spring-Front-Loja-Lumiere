0ea1e5c5bec092b4249c977c2272b89558005a64
Ancelmo <ancelmoaugusto15@gmail.com>
2026-01-27 22:49:32 -0300

    Primeiro commit do projeto

---
+++ backend/apilumiere/src/main/java/com/example/apilumiere/ApilumiereApplication.java
@@ -0,0 +1,13 @@
+package com.example.apilumiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilumiereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilumiereApplication.class, args);
+	}
+
+}
